06666a3c5ffaa752ec4df83e749bee36945bba65
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-29 16:03:16 -0300

    Remove addBtn and add function to add with enter pressed

--- src/index.js
+++ src/index.js
@@ -3,61 +3,53 @@ import './style.css';
 
 const taskInput1 = document.getElementById('task-input-1');
 const taskInput2 = document.getElementById('task-input-2');
-const addTaskBtn = document.getElementById('add-task-btn');
 const todoList = document.getElementById('todo-list');
+const clearCompletedButton = document.getElementById('clear-completed');
 
-const tasks = [];
+let tasks = [];
 
-function addTask(event) {
-  event.preventDefault();
-  const activeTaskInput = document.activeElement;
-  const taskInput = (activeTaskInput === taskInput1) ? taskInput1 : taskInput2;
-  const newTaskDescription = taskInput.value.trim();
-  if (newTaskDescription) {
-    const newTask = {
-      description: newTaskDescription,
-      completed: false,
-      index: tasks.length + 1
-    };
-    tasks.push(newTask);
-    renderTasks(todoList, tasks);
-    taskInput.value = '';
-  }
-}
+taskInput1.addEventListener("keydown", handleKeyPress);
+taskInput2.addEventListener("keydown", handleKeyPress);
 
-addTaskBtn.addEventListener('click', addTask);
+function handleKeyPress(event) {
+  if (event.key === 'Enter') {
+    const taskDescription = event.target.value.trim();
 
-function renderTasks(container, tasks) {
-  container.innerHTML = '';
-  for (let i = 0; i < tasks.length; i++) {
-    const task = tasks[i];
-    const taskItem = document.createElement('div');
-    taskItem.classList.add('task-item');
-    if (task.completed) {
-      taskItem.classList.add('completed');
+    if (taskDescription !== "" && taskDescription !== event.target.defaultValue) {
+      const newTask = {
+        description: taskDescription,
+        completed: false,
+        index: tasks.length + 1,
+      };
+      tasks.push(newTask);
+      event.target.value = "";
+      renderTasks();
     }
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = task.completed;
-    checkbox.addEventListener('change', function() {
-      task.completed = this.checked;
-      if (task.completed) {
-        taskItem.classList.add('completed');
-      } else {
-        taskItem.classList.remove('completed');
-      }
-    });
-    const description = document.createElement('span');
-    description.innerText = task.description;
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Delete';
-    deleteButton.addEventListener('click', function() {
-      tasks.splice(i, 1);
-      renderTasks(container, tasks);
-    });
-    taskItem.appendChild(checkbox);
-    taskItem.appendChild(description);
-    taskItem.appendChild(deleteButton);
-    container.appendChild(taskItem);
   }
 }
+
+function renderTasks() {
+  todoList.innerHTML = '';
+
+  tasks.forEach((task) => {
+    const taskItem = document.createElement('li');
+    taskItem.innerHTML = `
+      <input type="checkbox" ${task.completed ? 'checked' : ''}>
+      <span>${task.description}</span>
+    `;
+    taskItem.className = task.completed ? 'completed' : '';
+    todoList.appendChild(taskItem);
+
+    taskItem.querySelector('input[type="checkbox"]').addEventListener('click', () => {
+      task.completed = !task.completed;
+      taskItem.className = task.completed ? 'completed' : '';
+    });
+  });
+}
+
+function clearCompleted() {
+  tasks = tasks.filter((task) => !task.completed);
+  renderTasks();
+}
+
+clearCompletedButton.addEventListener('click', clearCompleted);
