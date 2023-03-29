2d41be921c35673227fcd757ef5024bc0ec7e09b
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-29 14:50:04 -0300

    Add basic structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const tasks = [
+  {
+    description: 'Task 1',
+    completed: false,
+    index: 1
+  },
+  {
+    description: 'Task 2',
+    completed: true,
+    index: 2
+  },
+  {
+    description: 'Task 3',
+    completed: false,
+    index: 3
+  }
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
+function renderTasks() {
+  const todoList = document.getElementById('todo-list');
+  const items = tasks
+    .sort((a, b) => a.index - b.index)
+    .map(({description, completed}) => {
+      const item = document.createElement('li');
+      item.textContent = description;
+      if (completed) {
+        item.classList.add('completed');
+      }
+      return item;
+    });
+  todoList.append(...items);
 }
 
-document.body.appendChild(component());
+renderTasks();
