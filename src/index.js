d0a69d498018c4508eb2bc3e04dd3274b53fe2ab
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-28 22:34:50 -0300

    Add src files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './style.css';
 
 
 function component() {
@@ -6,6 +7,7 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+     template: './src/index.html',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+  filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
