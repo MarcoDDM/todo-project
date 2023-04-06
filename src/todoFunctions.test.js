fb2b3432c2cad8760d1db6a98f9748e9a51724cf
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-04-06 14:08:24 -0300

    Fix eslint

--- src/todoFunctions.test.js
+++ src/todoFunctions.test.js
@@ -1,19 +1,15 @@
-const { addTask, deleteTask } = require('./todoFunctions');
+const { JSDOM } = require('jsdom');
 
-// Add mock implementation for localStorage
+const { document } = new JSDOM('<!doctype html><html><body></body></html>').window; const { addTask, deleteTask } = require('./todoFunctions.js');
+
 global.localStorage = {
   getItem: jest.fn(),
   setItem: jest.fn(),
   removeItem: jest.fn(),
 };
 
-// Add mock implementation for DOM
-const { JSDOM } = require('jsdom');
-const { document } = new JSDOM('<!doctype html><html><body></body></html>').window;
-
 global.document = document;
 global.window = document.defaultView;
-
 
 describe('addTask', () => {
   test('should add a task to the tasks array', () => {
