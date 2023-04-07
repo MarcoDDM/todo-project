3f8ddf08ed9c08ea6bc04c6a48c651572e9b1be1
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-04-06 21:58:57 -0300

    Add tests

--- src/tests/unitTest.test.js
+++ src/tests/unitTest.test.js
@@ -1,1 +1,30 @@
-import {updateTaskStatus, clearCompletedTasks} from './updateFunction.js';
+
+import {updateTaskStatus, clearCompletedTasks} from '../tests/unitTest.test.js';
+
+import { editTask, addTask } from '../todoFunctions.js';
+
+const { JSDOM } = require('jsdom');
+const dom = new JSDOM();
+global.document = dom.window.document;
+
+describe('addTask', () => {
+  test('should be defined', () => {
+    expect(addTask).toBeDefined();
+  });
+});
+
+describe('updateTaskStatus', () => {
+  test('should update the completed status of a task to true', () => {
+    const task = { id: 1, description: 'Task 1', completed: false };
+    const updatedTask = updateTaskStatus(task, true);
+    expect(updatedTask.completed).toBe(true);
+  });
+});
+
+describe('editTask', () => {
+  test('should throw an error when passed an invalid argument', () => {
+    const task = { id: 1, description: 'Task 1', completed: false };
+    const span = 'not an HTML element';
+    expect(() => editTask(task, span)).toThrow('Invalid argument: span must be an HTML element');
+  });
+});
