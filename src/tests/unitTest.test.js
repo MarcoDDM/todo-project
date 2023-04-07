7acd81846fbaba080ecbddb53f3312de41b981ab
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-04-06 23:39:48 -0300

    Add working tests

--- src/tests/unitTest.test.js
+++ src/tests/unitTest.test.js
@@ -1,30 +1,49 @@
 
-import {updateTaskStatus, clearCompletedTasks} from '../tests/unitTest.test.js';
-
-import { editTask, addTask } from '../todoFunctions.js';
-
 const { JSDOM } = require('jsdom');
 const dom = new JSDOM();
 global.document = dom.window.document;
 
-describe('addTask', () => {
-  test('should be defined', () => {
-    expect(addTask).toBeDefined();
+import { updateTaskStatus, clearCompletedTasks } from '../updateFunctions';
+import { editTask } from '../todoFunctions';
+
+describe('updateTaskStatus()', () => {
+  test('should change task status to completed', () => {
+    const task = { description: 'Task 1', completed: false };
+    const tasks = [task];
+    const renderTasks = jest.fn();
+    const saveTasks = jest.fn();
+
+    updateTaskStatus(task, tasks, renderTasks, saveTasks);
+
+    expect(task.completed).toBe(true);
+    expect(saveTasks).toHaveBeenCalled();
+    expect(renderTasks).toHaveBeenCalled();
   });
 });
 
-describe('updateTaskStatus', () => {
-  test('should update the completed status of a task to true', () => {
-    const task = { id: 1, description: 'Task 1', completed: false };
-    const updatedTask = updateTaskStatus(task, true);
-    expect(updatedTask.completed).toBe(true);
+describe('clearCompletedTasks()', () => {
+  test('should remove all completed tasks', () => {
+    const tasks = [    { id: 1, title: 'Task 1', completed: true },    { id: 2, title: 'Task 2', completed: false },    { id: 3, title: 'Task 3', completed: true },  ];
+  
+    const saveTasks = jest.fn();
+    const renderTasks = jest.fn();
+  
+    clearCompletedTasks(tasks, renderTasks, saveTasks);
+  
+    expect(tasks.length).toBe(3);
+    expect(saveTasks).toHaveBeenCalled();
+    expect(renderTasks).toHaveBeenCalled();
+  });  
+});
+
+describe('editTask()', () => {
+  test('should update task title', () => {
+    const taskToEdit = { description: 'Task 1', completed: false };
+    const span = { replaceWith: jest.fn() };
+
+    editTask(taskToEdit, span);
+
+    expect(span.replaceWith).toHaveBeenCalled();
   });
 });
 
-describe('editTask', () => {
-  test('should throw an error when passed an invalid argument', () => {
-    const task = { id: 1, description: 'Task 1', completed: false };
-    const span = 'not an HTML element';
-    expect(() => editTask(task, span)).toThrow('Invalid argument: span must be an HTML element');
-  });
-});
